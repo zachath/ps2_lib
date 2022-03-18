6b06de47588c3c3667940fe79ef142e1ab488b5f
zachath <zachariasthorell@gmail.com>
2022-03-18 17:41:21 +0100

    online status added to lookup.

--- src/main/java/CensusAPIUtil.java
+++ src/main/java/CensusAPIUtil.java
@@ -14,7 +14,10 @@ public class CensusAPIUtil {
     public static final String FILTER_PREFIX = AND + "c:";
     public static final String SHOW_FILTER = FILTER_PREFIX + "show=";
 
-    public static final String GET_CHARACTER = BASE_URL + "get/" + NAMESPACE + "character/?name.first_lower=";
+    public static final String GET = "get/";
+
+    public static final String GET_CHARACTER = BASE_URL + GET + NAMESPACE + "character/?name.first_lower=";
+    public static final String GET_CHARACTER_ONLINE_STATUS = BASE_URL + GET + NAMESPACE + "characters_online_status/?character_id=";
 
     public static final Map<Integer, String> FACTION_MAP = Map.of(1, "TR", 2, "NC", 3, "VS");
     public static final Map<String, String> FACTION_COLOUR_MAP = Map.of(FACTION_MAP.get(1), Util.ANSI_RED,FACTION_MAP.get(2), Util.ANSI_BLUE, FACTION_MAP.get(3), Util.ANSI_PURPLE);
--- src/main/java/LookupTask.java
+++ src/main/java/LookupTask.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  * The task of looking up a player
  */
 public class LookupTask implements Runnable {
-    private static final String FILTERS = CensusAPIUtil.SHOW_FILTER + "name,faction_id,times,certs,battle_rank";
+    private static final String FILTERS = CensusAPIUtil.SHOW_FILTER + "character_id,name,faction_id,times,certs,battle_rank";
 
     private final String player;
     public LookupTask(String player) {
@@ -38,6 +38,25 @@ public class LookupTask implements Runnable {
                 return;
             }
 
+            String playerId = responseObject.getString("character_id");
+            String isOnlineQuery = CensusAPIUtil.GET_CHARACTER_ONLINE_STATUS + playerId;
+
+            connection = new URL(isOnlineQuery).openConnection();
+            response = connection.getInputStream();
+            scanner = new Scanner(response);
+
+            String onlineResponse = scanner.useDelimiter("\\A").next();
+            JSONObject responseObjectID;
+            try {
+                responseObjectID = new JSONObject(onlineResponse).getJSONArray("characters_online_status_list").getJSONObject(0);
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return;
+            }
+
+            String idResponse = responseObjectID.getString("online_status");
+            boolean isOnline = idResponse.equals("1");
+
             int minutesPlayed = Integer.parseInt((String) responseObject.getJSONObject("times").get("minutes_played"));
             double hoursPlayed = (double) minutesPlayed / 60;
 
@@ -50,7 +69,8 @@ public class LookupTask implements Runnable {
             //Builder not needed but looks cleaner than massive String and more concise than many prints.
             StringBuilder builder = new StringBuilder();
             builder.append("Name: ").append(responseObject.getJSONObject("name").get("first")).append("\t");
-            builder.append("Faction: ").append(Util.printInColour(faction, CensusAPIUtil.FACTION_COLOUR_MAP.get(faction))).append("\n").append("\n");
+            builder.append("Faction: ").append(Util.printInColour(faction, CensusAPIUtil.FACTION_COLOUR_MAP.get(faction))).append("\n");
+            builder.append("Online Status: ").append(isOnline).append("\n");
 
             builder.append("Logins: ").append(responseObject.getJSONObject("times").get("login_count")).append("\n").append("\n");
 
